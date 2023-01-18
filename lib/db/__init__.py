ae995a1184fc6a531b757470cceb5982cb404e68
Grant <gccody2010@live.com>
2023-01-18 01:05:58 -0500

    Db rewrite
    No longer need build.sql. tables are created with dictionaries in db

--- lib/db/__init__.py
+++ lib/db/__init__.py
@@ -4,18 +4,67 @@ from sqlite3 import Connection, Cursor, connect
 
 class DB:
     DB_PATH = "./data/database.db"
-    BUILD_PATH = "./data/build.sql"
     cxn: Connection
     cur: Cursor
+
+    tables: dict[str, dict[str, str]] = {
+        'bans': {
+            'guild_id': 'TEXT',
+            'user_id': 'TEXT',
+            'mod_id': 'TEXT',
+            'reason': 'TEXT',
+            'ts': 'INTEGER'
+        },
+        'guilds': {
+            'id': 'TEXT PRIMARY KEY NOT NULL',
+            'l_id': 'TEXT',
+            'a_id': 'TEXT'
+        },
+        'reports': {
+            'guild_id': 'TEXT',
+            'user_id': 'TEXT',
+            'target_id': 'TEXT',
+            'report': 'TEXT'
+        },
+        'commands': {
+            'command_name': 'TEXT',
+            'guild_id': 'TEXT',
+            'user_id': 'TEXT',
+            'params': 'JSON',
+            'ts': 'INTEGER'
+        },
+        'levels': {
+            'guild_id': 'TEXT',
+            'user_id': 'TEXT',
+            'xp': 'INTEGER'
+        }
+    }
 
     def __init__(self):
         self.cxn = connect(self.DB_PATH, check_same_thread=False)
         self.cur = self.cxn.cursor()
 
-    def build(self) -> None:
-        if isfile(self.BUILD_PATH):
-            self.scriptexec(self.BUILD_PATH)
+    @staticmethod
+    def diff(list1, list2):
+        return list(set(list1).symmetric_difference(set(list2)))
 
+    def update_table(self, table: str, columns_str_list: list[str], columns_list: dict[str, str]):
+        self.cur.execute(f"CREATE TABLE {table}_new ({' '.join(columns_str_list)});")
+        self.cur.execute(f"INSERT INTO {table}_new SELECT * FROM {table};")
+        self.cur.execute(f"DROP TABLE {table};")
+        self.cur.execute(f"ALTER TABLE {table}_new RENAME TO {table}")
+
+    def build(self) -> None:
+        for table, columns in self.tables.items():
+            # Create tables
+            columns_str_list = []
+            for index, (name, data_type) in enumerate(columns.items()):
+                columns_str_list.append(f"{name} {data_type}," if index < len(columns) - 1 else f"{name} {data_type}")
+
+            self.cur.execute(f"CREATE TABLE IF NOT EXISTS {table} ({' '.join(columns_str_list)});")
+
+            # Update tables in case the types need to be updated
+            self.update_table(table, columns_str_list, columns)
         self.commit()
 
     def commit(self) -> None:
@@ -47,12 +96,6 @@ class DB:
 
     def execute(self, command, *values) -> None:
         self.cur.execute(command, tuple(values))
-        self.commit()
 
     def multiexec(self, command, valueset) -> None:
         self.cur.execute(command, valueset)
-        self.commit()
-
-    def scriptexec(self, path) -> None:
-        with open(path, "r", encoding='utf-8') as script:
-            self.cur.executescript(script.read().__str__())
