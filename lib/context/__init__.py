a6a34c439648d499f33061976c00ca03718c5c45
Grant <gccody2010@live.com>
2023-01-18 01:03:45 -0500

    No images

--- lib/context/__init__.py
+++ lib/context/__init__.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import random
+
+import discord
 from discord import ApplicationContext, Interaction, WebhookMessage
 from discord.embeds import Embed
 
@@ -9,9 +12,12 @@ class CustomContext(ApplicationContext):
     async def respond(self, *args, **kwargs) -> Interaction | WebhookMessage:
         if kwargs.get('embed'):
             embed: Embed = kwargs['embed']
-            embed.set_footer(text="Made by Gccody", icon_url=self.logo_path)
+            if not isinstance(embed.colour, discord.Colour):
+                color = int("0x"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]), 16)
+                embed.colour = color
+            embed.set_footer(text="Made by Gccody")
             embed.timestamp = datetime.now()
-            embed.set_thumbnail(url=self.logo_path)
+            # embed.set_thumbnail(url=self.logo_path)
             kwargs['embed'] = embed
             return await super().respond(*args, **kwargs)
         else:
